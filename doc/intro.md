f4364aa12d687c10a586fed2d23a51f0aca9ac90
lindenes <dodigrams@gmail.com>
2025-01-18 15:21:21 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to health-samurai-test-task
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/health_samurai_test_task/health_samurai_test_task_test.clj
@@ -0,0 +1,7 @@
+(ns health-samurai-test-task.health-samurai-test-task-test
+  (:require [clojure.test :refer :all]
+            [health-samurai-test-task.health-samurai-test-task :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
